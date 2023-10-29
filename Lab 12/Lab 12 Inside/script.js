519f6e0b2d017726890693c91a3ebe2a8437d5b6
mmt86 <146208774+mmt86@users.noreply.github.com>
2023-10-29 16:44:58 -0400

    Create script.js

---
+++ Lab 12/Lab 12 Inside/script.js
@@ -0,0 +1,8 @@
+var initLoad = true;
+var layerTypes = {
+    'fill': ['fill-opacity'],
+    'line': ['line-opacity'],
+    'circle': ['circle-opacity', 'circle-stroke-opacity'],
+    'symbol': ['icon-opacity', 'text-opacity'],
+  }
+    etc
